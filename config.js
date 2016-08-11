e26dad4ed945d7962a7f4bdb7657676003fac858
= <benjamin.kandov@stonybrook.edu>
2016-08-11 11:37:45 -0400

    grg

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'secret' : 'benloves',
 	'database':'mongodb://127.0.0.1:27017/test',
-	 'databaseMongo':'mongodb://<dbuser>:<dbpassword>@ds153745.mlab.com:53745/heroku_mbtxb4mz'
+	 'databaseMongo':'mongodb://<ben:1234@ds153745.mlab.com:53745/heroku_mbtxb4mz'
 
 };
